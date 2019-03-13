8fa3d9c6f56fdb67ed974fea76bb731ab664e82e
陈博洋 <chenby@zylliondata.com>
2019-03-13 14:28:25 +0800

    :herb: Modify thymeleaf

--- src/main/java/at/lingu/laravelmix/web/HomeController.java
+++ src/main/java/at/lingu/laravelmix/web/HomeController.java
@@ -2,12 +2,15 @@ package at.lingu.laravelmix.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String welcome() {
-        return "welcome";
+    public ModelAndView welcome() {
+        ModelAndView mv = new ModelAndView("welcome");
+        mv.addObject("greeting", "Spring Boot and Laravel Mix");
+        return mv;
     }
 }
